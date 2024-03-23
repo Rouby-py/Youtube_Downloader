15662ee996c2673709dfca4750021eb3c77e54ec
Rouby-py <abdullahm.elrouby@gmail.com>
2024-03-24 01:35:53 +0200

    Download Mechanism

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ from io import BytesIO
 from pytube import *
 from requests import *
 from tkinter import *
+from tkinter import filedialog, ttk
 from PIL import ImageTk, Image
 from urllib.request import *
-from tkinter import ttk
 from PIL import *
 import requests
 
@@ -27,7 +27,26 @@ f1.grid(row=0, column=0, sticky="news")
 f2 = Frame(root, bg='gray')
 f2.grid(row=0, column=0, sticky="news")
 
+def get_path():
+    destination_path = filedialog.askdirectory(title="Select Destination Folder")
+    if destination_path:
+        return destination_path
+    else:
+        invalidPath.place(x=70, y=235);
 
+def download(req_resolution):
+    path=get_path()
+    print(path)
+    yt = YouTube(link)
+    streams = yt.streams
+    for stream in streams:
+        if stream.resolution == req_resolution:
+            stream.download(path)
+            break
+
+def get_value():
+    res = cb.get()
+    download(res)
 def f2load(video_title=None, thumbnail_url=None, download_options=None):
     # video title
     title = ttk.Label(f2, text=video_title, background='gray', font=('Helvetica', 24, 'bold'))
@@ -44,25 +63,26 @@ def f2load(video_title=None, thumbnail_url=None, download_options=None):
     thumbnail.image = img
     thumbnail.pack()
 
+
     # drop down menu for quality selection
 
-    cb = ttk.Combobox(f2, values=download_options, width=40, state='readonly')
+    cb.config(values = download_options)
     cb.pack()
-
+    download_button = Button(f2, text="Download", font=("Raavi", 26, "bold"), background="green", command = get_value)
+    download_button.pack()
 
 
 def get_data(link):
     try:
         yt = YouTube(link)
         yt.check_availability()
-        invalid.place_forget()  # remove warning message
+        invalidLink.place_forget()  # remove warning message
         f2.tkraise()
 
         title = yt.title
         thumbnail = yt.thumbnail_url
         try:
             streams = yt.streams
-            print(streams)
             res_names = set()
             for stream in streams:
                 if stream.resolution:
@@ -70,13 +90,15 @@ def get_data(link):
             res_values = list()
             for res in res_names:
                 res_values.append(int(res.split('p')[0]))
-            f2load(title, thumbnail, sorted(res_values))
+            res_values=sorted(res_values)
+            res_values = [str(res) + 'p' for res in res_values]
+            f2load(title, thumbnail, res_values)
         except Exception as e:
-            print(e)
-            f2load(title, thumbnail)
+            print("ERRORRR")
+            # f2load(title, thumbnail)
 
     except:  # put warning message
-        invalid.place(x=70, y=235)
+        invalidLink.place(x=70, y=235)
 
 
 def click(event=None):
@@ -85,14 +107,16 @@ def click(event=None):
     # entry.delete(0, END)
 
 
+
+
 # Warning message for invalid links
-invalid = Label(f1, text="Please enter a valid link", font="Raavi", foreground="darkred", background='gray')
-
-
+invalidLink = Label(f1, text="Please enter a valid link", font="Raavi", foreground="darkred", background='gray')
+invalidPath = Label(f1, text="Please select a valid path", font="Raavi", foreground="darkred", background='gray')
+link = str()
 # Just a header
 label = Label(f1, text="Youtube Downloader", font=("Raavi", 26, "bold"), background="gray")
 label.pack(side=TOP, pady=60)
-
+cb = ttk.Combobox(f2, width=40, state='readonly')
 
 # Entry box for link
 entry = Entry(f1, width=50, font=("Raavi", 20, "bold"))
